07c2d8b0e8770327abc7ed59d0e8b0a2adf93baa
Marton <martondeveloper@gmail.com>
2019-05-21 22:40:20 +0200

    Galery

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,86 @@ jQuery(function($) {
   });
 
 });
+
+function openImage(number) {
+
+  var pswpElement = document.querySelectorAll('.pswp')[0];
+
+  var items = [
+
+    {
+
+      src: "assets/img/galery/1.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/2.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/3.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/4.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/5.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/6.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/7.png",
+      w: 600,
+      h: 400
+
+    },
+
+    {
+
+      src: "assets/img/galery/8.png",
+      w: 600,
+      h: 400
+
+    }
+
+  ];
+
+  var options = {
+
+    index: number
+
+  };
+
+  var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+  gallery.init();
+
+}
